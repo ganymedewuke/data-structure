8b1e739bdb7d7660ad2e806cbe604f48242de04f
ganymedewuke <javashellsql1984>
2020-12-12 23:52:39 +0800

    移除链表元素

---
+++ src/main/java/com/ganymede/link/移除链表元素.java
@@ -0,0 +1,64 @@
+package com.ganymede.link;
+
+/**
+ * 删除链表中等于给定值 val 的所有节点。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->6->3->4->5->6, val = 6
+ * 输出: 1->2->3->4->5
+ */
+public class 移除链表元素 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    public static ListNode removeElements(ListNode head, int val) {
+
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+
+        if (head == null)
+            return null;
+
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+        // 1->2->6->3->4->5->6
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(6);
+        listNode.next.next.next = new ListNode(3);
+        listNode.next.next.next.next = new ListNode(4);
+        listNode.next.next.next.next.next = new ListNode(5);
+        listNode.next.next.next.next.next.next = new ListNode(6);
+
+
+        System.out.println(listNode);
+
+        removeElements(listNode, 6);
+
+        System.out.println(listNode);
+
+    }
+}
---
+++ src/main/java/com/ganymede/link/移除链表元素虚拟头节点.java
@@ -0,0 +1,57 @@
+package com.ganymede.link;
+
+/**
+ * 删除链表中等于给定值 val 的所有节点。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->6->3->4->5->6, val = 6
+ * 输出: 1->2->3->4->5
+ */
+public class 移除链表元素虚拟头节点 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    public static ListNode removeElements(ListNode head, int val) {
+
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+
+        return dummyHead.next;
+    }
+
+
+    public static void main(String[] args) {
+        // 1->2->6->3->4->5->6
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(6);
+        listNode.next.next.next = new ListNode(3);
+        listNode.next.next.next.next = new ListNode(4);
+        listNode.next.next.next.next.next = new ListNode(5);
+        listNode.next.next.next.next.next.next = new ListNode(6);
+
+
+        System.out.println(listNode);
+
+        removeElements(listNode, 6);
+
+        System.out.println(listNode);
+
+    }
+}
