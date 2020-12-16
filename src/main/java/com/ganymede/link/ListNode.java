518b50d845d467d1ab29d95dce9a1b42ce658233
ganymedewuke <ganymede.wuke@foxmail.com>
2020-12-16 18:37:58 +0800

    注解toString

--- src/main/java/com/ganymede/link/ListNode.java
+++ src/main/java/com/ganymede/link/ListNode.java
@@ -1,19 +1,14 @@
 package com.ganymede.link;
 
+import lombok.ToString;
+
+@ToString
 public class ListNode {
     int val;
     ListNode next;
 
     ListNode(int x) {
         val = x;
-    }
-
-    @Override
-    public String toString() {
-        return "ListNode{" +
-                "val=" + val +
-                ", next=" + next +
-                '}';
     }
 
     /**
