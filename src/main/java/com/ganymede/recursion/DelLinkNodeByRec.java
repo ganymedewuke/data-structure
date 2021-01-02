637378802a7e1771870ef2634e3b74baef71dddf
ganymedewuke <javashellsql1984>
2021-01-02 16:19:30 +0800

    递归过程分析

--- src/main/java/com/ganymede/recursion/DelLinkNodeByRec.java
+++ src/main/java/com/ganymede/recursion/DelLinkNodeByRec.java
@@ -11,23 +11,55 @@ public class DelLinkNodeByRec {
      * ListNode(int x) { val = x; }
      * }
      */
-    public static ListNode removeElements(ListNode head, int val) {
-        if (head == null)
-            return null;
+    public ListNode removeElements(ListNode head, int val, int depth) {
+        String depthString = generateDepthString(depth);
 
-        head.next = removeElements(head.next, val);
-        return head.val == val ? head.next : head;
+        System.out.print(depthString);
+        System.out.println("Call : reomve " + val + " in " + head);
+
+        if (head == null) {
+            System.out.print(depthString);
+            System.out.println("Return : " + head);
+            return null;
+        }
+
+
+        ListNode res = removeElements(head.next, val, depth + 1);
+        System.out.print(depthString);
+        System.out.println("After remove " + val + " : " + res);
+
+        ListNode ret;
+        if (head.val == val)
+            ret = res;
+        else {
+            head.next = res;
+            ret = head;
+        }
+        System.out.print(depthString);
+        System.out.println("Return : " + head);
+
+        return ret;
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuffer res = new StringBuffer();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
     }
 
     public static void main(String[] args) {
         // 1->2->6->3->4->5->6
-        int[] nums = {1, 4, 2, 6, 3, 4, 5, 6};
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
 
         ListNode listNode = new ListNode(nums);
 
         System.out.println(listNode);
 
-        removeElements(listNode, 4);
+        DelLinkNodeByRec delLinkNodeByRec = new DelLinkNodeByRec();
+
+        delLinkNodeByRec.removeElements(listNode, 6, 0);
         System.out.println(listNode);
     }
 }
